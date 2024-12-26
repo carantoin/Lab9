abb86baf33c207bcfb4d22cbc3c406c5c560edde
carantoin <90127329+carantoin@users.noreply.github.com>
2024-12-26 16:12:56 +0200

    Добавьте файлы проекта.

---
+++ Lab9/Price.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    internal class Price
+    {
+        private double coins;
+        public double Coins
+        {
+            get => coins;
+            set { if (value > 0) coins = value; }
+        }
+        private double amount;
+        public double Amount
+        {
+            get => amount;
+            set { if (value > 0) amount = value; }
+        }
+        public Price(double coins, double a)
+        {
+            this.coins = coins;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"");
+        }
+        public double GetPrice()
+        {
+            return coins * amount;
+        }
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,8 @@
+﻿using Lab9;
+
+Console.Write("Введите цену товара:");
+double c = double.Parse(Console.ReadLine());
+Console.Write("Введите количество единиц товара:");
+double a = double.Parse(Console.ReadLine());
+Price price = new Price(c, a);
+price.Print();
